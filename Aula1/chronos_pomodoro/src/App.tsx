2ee9b319532bde5d6b1e242718c30c51e8b6611f
Joao Henrique <joaohenrique9021@gmail.com>
2025-03-28 18:46:34 -0300

    Organizando component e criando a primeira seção

--- Aula1/chronos_pomodoro/src/App.tsx
+++ Aula1/chronos_pomodoro/src/App.tsx
@@ -1,12 +1,17 @@
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+
 import './styles/global.css';
 import './styles/theme.css';
-import { Container } from './components/Container/index';
-import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Heading>
+          <Logo />
+        </Heading>
       </Container>
       <Container>
         <Heading>MENU</Heading>
--- Aula1/chronos_pomodoro/src/components/Container/index.tsx
+++ Aula1/chronos_pomodoro/src/components/Container/index.tsx
@@ -1,1 +1,13 @@
-export { Container } from "./Container"
+import styles from "./styles.module.css"
+
+type containerProps = {
+    children: React.ReactNode
+}
+
+export function Container({ children }: containerProps) {
+    return (
+    <div className={styles.container}>
+            <div className={styles.content}>{children}</div>
+        </div>
+    )
+}
