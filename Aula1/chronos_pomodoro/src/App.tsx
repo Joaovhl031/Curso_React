dc0a26b7bf567d468c1fbdd31a9879c58020fd26
Joao Henrique <joaohenrique9021@gmail.com>
2025-03-30 23:18:12 -0300

    Seção forms

--- Aula1/chronos_pomodoro/src/App.tsx
+++ Aula1/chronos_pomodoro/src/App.tsx
@@ -4,6 +4,9 @@ import { Container } from './components/Container';
 import './styles/global.css';
 import './styles/theme.css';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -14,7 +17,29 @@ export function App() {
         </Heading>
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu></Menu>
+      </Container>
+      <Container>
+        <CountDown></CountDown>
+      </Container>
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <DefaultInput type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
