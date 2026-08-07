245da8d0799f88852e2ed74c6ee42791150c9312
Joao Henrique <joaohenrique9021@gmail.com>
2026-08-06 23:38:05 -0300

    Adiciona lógica para criação de novas tarefas e gerenciamento de ciclos no MainForm

--- Aula1/chronos_pomodoro/src/components/MainForm/index.tsx
+++ Aula1/chronos_pomodoro/src/components/MainForm/index.tsx
@@ -3,12 +3,52 @@ import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
+import { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../context/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
 
 export function MainForm() {
+  const { state,setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    if(taskNameInput.current === null) return;
+    
+    const taskName = taskNameInput.current.value.trim();
+    if (taskName === '') {
+      alert('Por favor, digite o nome da tarefa.');
+      return;
+    }
+
+    const newTask: TaskModel= {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.task, newTask],
+      };
+    });
   }
 
   return [
--- Aula1/chronos_pomodoro/src/models/TaskStateModel.ts
+++ Aula1/chronos_pomodoro/src/models/TaskStateModel.ts
@@ -5,7 +5,7 @@ export type TaskStateModel = {
   secondsRemaining: number;
   formattedSecondsRemaining: string;
   activeTask: TaskModel | null;
-  currentCycle: number | null;
+  currentCycle: number;
   config: {
     workTime: number;
     shortBreakTime: number;
