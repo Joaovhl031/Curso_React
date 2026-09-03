8a093dd69d5b797f1e66a9fc98a45388773d1df0
Joao Henrique <joaohenrique9021@gmail.com>
2026-09-02 21:08:13 -0300

    Adiciona lógica de contagem regressiva e gerenciamento de ciclos, incluindo formatação de segundos para minutos

--- Aula1/chronos_pomodoro/src/components/CountDown/index.tsx
+++ Aula1/chronos_pomodoro/src/components/CountDown/index.tsx
@@ -4,8 +4,7 @@ import styles from './styles.module.css';
 
 
 export function CountDown() {
-  const taskContext = useTaskContext();
-  console.log('taskContext', taskContext);
+  const { state } = useTaskContext();
 
-  return <div className={styles.container}>00:00</div>;
+  return <div className={styles.container}>{state.formattedSecondsRemaining}</div>;
 }
--- Aula1/chronos_pomodoro/src/components/MainForm/index.tsx
+++ Aula1/chronos_pomodoro/src/components/MainForm/index.tsx
@@ -1,4 +1,4 @@
-import { PlayCircleIcon } from 'lucide-react';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
@@ -7,9 +7,10 @@ import { TaskModel } from '../../models/TaskModel';
 import { useTaskContext } from '../../context/TaskContext/useTaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function MainForm() {
-  const { state,setState } = useTaskContext();
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   const nextCycle = getNextCycle(state.currentCycle);
@@ -18,21 +19,21 @@ export function MainForm() {
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    if(taskNameInput.current === null) return;
-    
+    if (taskNameInput.current === null) return;
+
     const taskName = taskNameInput.current.value.trim();
     if (taskName === '') {
       alert('Por favor, digite o nome da tarefa.');
       return;
     }
 
-    const newTask: TaskModel= {
+    const newTask: TaskModel = {
       id: Date.now().toString(),
       name: taskName,
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
+      duration: state.config[nextCycleType],
       type: nextCycleType,
     };
 
@@ -45,21 +46,32 @@ export function MainForm() {
         activeTask: newTask,
         currentCycle: nextCycle,
         secondsRemaining,
-        formattedSecondsRemaining: '00:00',
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
         tasks: [...prevState.task, newTask],
       };
     });
   }
 
+  function handleStopTask() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: '00:00',
+      };  
+    });
+  }
   return [
-    <form onSubmit={ handleCreateNewTask } className='form'>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
           id='meuInput'
           type='text'
           placeholder='Digite algo'
-          ref={ taskNameInput }
+          ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
@@ -69,11 +81,32 @@ export function MainForm() {
         </p>
       </div>
 
+      {state.currentCycle > 0 && (
+        <div className='formRow'>
+          <Cycles />
+        </div>
+      )}
+
       <div className='formRow'>
-        <Cycles />
-      </div>
-      <div className='formRow'>
-        <DefaultButton icon={<PlayCircleIcon />} color='green' />
+        {!state.activeTask ? (
+          <DefaultButton
+            aria-label='Iniciar nova tarefa'
+            title='Iniciar nova tarefa'
+            type='submit'
+            icon={<PlayCircleIcon />}
+            key='startTaskButton'
+          />
+        ) : (
+          <DefaultButton
+            aria-label='Parar tarefa'
+            title='Parar tarefa'
+            color='red'
+            type='button'
+            icon={<StopCircleIcon />}
+            onClick={handleStopTask}
+            key='stopTaskButton'
+          />
+        )}
       </div>
     </form>,
   ];
